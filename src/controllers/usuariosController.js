3b87ea7006e2426da920cffefe8bcddb1bfccb17
Kutik-Gomez <kutikgomez@gmail.com>
2024-05-22 09:55:24 -0500

    crud de usuarios

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -2,8 +2,45 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 export const consultar = async (req, res) => {
-    const consulta = await prisma.usuarios.findMany()
-    res.json(consulta);
+  const consulta = await prisma.usuarios.findMany();
+  res.json(consulta);
+};
+
+export const guardar = async (req, res) => {
+  const crear = await prisma.usuarios.create({
+    data: req.body,
+  });
+  res.json(crear);
+};
+
+export const actualizar = async (req, res) => {
+  const actualiza = await prisma.usuarios.update({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: req.body,
+  });
+  res.json(actualiza);
+};
+
+export const eliminar = async (req, res) => {
+  const elimina = await prisma.usuarios.delete({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  res.json(elimina);
+};
+
+export const buscador = async (req, res) => {
+  const nombre = req.params.nombre.toLowerCase();
+  const busca = await prisma.usuarios.findMany({
+    where: {
+      nombre: {
+        contains: nombre,
+      },
+    },
+  });
+  res.json(busca);
 };
