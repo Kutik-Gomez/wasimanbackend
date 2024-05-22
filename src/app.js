7f1105c9cdf74315eeb3c16bb72d78de62b3de59
Kutik-Gomez <kutikgomez@gmail.com>
2024-05-22 12:54:02 -0500

    login usuario

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 
 import express from 'express';
 import usuariosRoutes from './routes/usuarios.routes.js'
+import autoRoutes from './routes/auto.routes.js';
 
 const app = express();
 
 app.use(express.json());
 //Rutas
 app.use('/api', usuariosRoutes)
-
+app.use('/auth', autoRoutes);
 
 
 
